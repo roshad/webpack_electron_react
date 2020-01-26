33179404ce71e402f8215289e1fb0bb616e308af
roshad <roshad87a9@gmail.com>
2020-01-26 21:07:41 +0800

    Can't resolve './src'

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = [
   {
     mode: "development",    
-    target: "electron-renderer",
-    plugins: [
-      new HtmlWebpackPlugin({
-        
-      })
-    ]  
+    target: "electron-main",
+    
     
   }
 ];
